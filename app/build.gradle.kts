3c58718b94c3dea1129bc98aa49f21ebb6f86491
suhas-koheda <sharmasuhas450@gmail.com>
2025-04-11 21:45:09 +0530

    feat: Implement IP broadcasting and update server endpoints
    - Add IP broadcasting functionality using `IpBroadcaster` to send the device's local IP address to the network on port 9002.
    - Add internet and network access permissions to AndroidManifest.xml.
    - Add a new `GET` endpoint `/` to the Ktor server to return the request URI.
    - Update the `runHearSocket` path to `/hear`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,5 +68,6 @@ dependencies {
     implementation(libs.ktor.server.websockets)
     implementation(libs.ktor.client.java)
     implementation(libs.ktor.network)
+    implementation(libs.richeditor.compose)
 
 }
